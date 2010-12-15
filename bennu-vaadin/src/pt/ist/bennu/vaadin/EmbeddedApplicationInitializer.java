ad653783413a5367258ca8fbce438af0803fbf73
João Marques <joao.marques@ist.utl.pt>
2010-12-15 15:31:34 +0000

    New Feature: Added EmbeddedComponent annotation for vaddin components.

--- bennu-vaadin/src/pt/ist/bennu/vaadin/EmbeddedApplicationInitializer.java
+++ bennu-vaadin/src/pt/ist/bennu/vaadin/EmbeddedApplicationInitializer.java
@@ -2,8 +2,10 @@ package pt.ist.bennu.vaadin;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.Map.Entry;
 import java.util.Properties;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
@@ -13,15 +15,21 @@ import javax.servlet.http.HttpServlet;
 
 import pt.ist.vaadinframework.EmbeddedApplication;
 import pt.ist.vaadinframework.ui.EmbeddedComponentContainer;
+import pt.utl.ist.fenix.tools.util.FileUtils;
+import vaadin.annotation.EmbeddedAnnotationProcessor;
+import vaadin.annotation.EmbeddedComponent;
 
 @SuppressWarnings("serial")
 public class EmbeddedApplicationInitializer extends HttpServlet {
+    private static final Set<Class<? extends EmbeddedComponentContainer>> embeddedComponentClasses = new HashSet<Class<? extends EmbeddedComponentContainer>>();
+
     @Override
     public void init(ServletConfig config) throws ServletException {
 	super.init(config);
+
 	try {
 	    Properties properties = new Properties();
-	    final InputStream inputStream = getClass().getResourceAsStream("/embeddedcomponent.properties");
+	    InputStream inputStream = getClass().getResourceAsStream("/embeddedcomponent.properties");
 	    if (inputStream != null) {
 		properties.load(inputStream);
 		for (Entry<Object, Object> entry : properties.entrySet()) {
@@ -39,6 +47,48 @@ public class EmbeddedApplicationInitializer extends HttpServlet {
 	} catch (IOException e) {
 	    e.printStackTrace();
 	    throw new Error("Could not read property file: embeddedcomponent.properties");
+	}
+
+	loadEmbeddedComponentsFromFile(embeddedComponentClasses);
+	for (Class<? extends EmbeddedComponentContainer> embeddedComponentClass : embeddedComponentClasses) {
+	    EmbeddedComponent embeddedComponent = embeddedComponentClass.getAnnotation(EmbeddedComponent.class);
+	    if (embeddedComponent == null) {
+		continue;
+	    }
+
+	    String[] paths = embeddedComponent.path();
+
+	    for (String path : paths) {
+		try {
+		    EmbeddedApplication.addResolutionPattern(Pattern.compile(path), embeddedComponentClass);
+		} catch (PatternSyntaxException e) {
+		    throw new Error("Error interpreting pattern: " + path, e);
+		}
+	    }
+
+	}
+    }
+
+    private void loadEmbeddedComponentsFromFile(final Set<Class<? extends EmbeddedComponentContainer>> embeddedComponentClasses) {
+	final InputStream inputStream = this.getClass().getResourceAsStream("/" + EmbeddedAnnotationProcessor.LOG_FILENAME);
+	if (inputStream != null) {
+	    try {
+		final String contents = FileUtils.readFile(inputStream);
+		for (final String classname : contents.split(EmbeddedAnnotationProcessor.ENTRY_SEPERATOR)) {
+		    try {
+			ClassLoader loader = Thread.currentThread().getContextClassLoader();
+			Class<? extends EmbeddedComponentContainer> type = (Class<? extends EmbeddedComponentContainer>) loader
+				.loadClass(classname);
+			embeddedComponentClasses.add(type);
+		    } catch (final ClassNotFoundException e) {
+			e.printStackTrace();
+		    }
+		}
+	    } catch (final IOException e) {
+		e.printStackTrace();
+	    }
+	} else {
+	    throw new Error("Error opening file: " + EmbeddedAnnotationProcessor.LOG_FILENAME);
 	}
     }
 }
