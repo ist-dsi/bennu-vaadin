0fb8ddad06c404e3f0562adcbdcb9ff7a5ea9b44
Luis Cruz <luis.cruz@ist.utl.pt>
2011-01-11 17:42:38 +0000

    Minor Change: Allow vaadin features to controle entire html page.

--- bennu-vaadin/src/pt/ist/bennu/vaadin/actions/VaadinContextAction.java
+++ bennu-vaadin/src/pt/ist/bennu/vaadin/actions/VaadinContextAction.java
@@ -3,6 +3,7 @@ package pt.ist.bennu.vaadin.actions;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import myorg.presentationTier.Context;
 import myorg.presentationTier.LayoutContext;
 import myorg.presentationTier.actions.ContextBaseAction;
 
@@ -13,13 +14,25 @@ import org.apache.struts.action.ActionMapping;
 import pt.ist.fenixWebFramework.struts.annotations.Mapping;
 import pt.ist.vaadinframework.EmbeddedApplication;
 
-@Mapping(path="/vaadinContext")
+@Mapping(path = "/vaadinContext")
 public class VaadinContextAction extends ContextBaseAction {
 
     public static class VaadinLayoutContext extends LayoutContext {
 	public VaadinLayoutContext(String path) {
 	    super(path);
 	    addHead("/embedded/head.jsp");
+	}
+    }
+
+    public static class FullVaadinLayoutContext extends Context {
+
+	@Override
+	public ActionForward forward(final String body) {
+	    return new ActionForward("/embedded/head.jsp");
+	}
+
+	public FullVaadinLayoutContext() {
+	    super(null);
 	}
     }
 
@@ -35,19 +48,11 @@ public class VaadinContextAction extends ContextBaseAction {
 	return forwardToVaadin(request, argument, false);
     }
 
-    public static ActionForward forwardToVaadin(final HttpServletRequest request, final String argument, final boolean useBennuLayout) {
+    public static ActionForward forwardToVaadin(final HttpServletRequest request, final String argument,
+	    final boolean useBennuLayout) {
 	request.getSession().setAttribute(EmbeddedApplication.VAADIN_PARAM, argument);
-	final VaadinLayoutContext vaadinLayoutContext = new VaadinLayoutContext(getContext(request).getPath());
-	if (!useBennuLayout) {
-	    vaadinLayoutContext.setPageHeader("/layout/blank.jsp");
-	    vaadinLayoutContext.setSideBar("/layout/blank.jsp");
-	    vaadinLayoutContext.setMenuTop("/layout/blank.jsp");
-	    vaadinLayoutContext.setSubMenuTop("/layout/blank.jsp");
-	    vaadinLayoutContext.setPageOperations("/layout/blank.jsp");
-	    vaadinLayoutContext.setBreadCrumbs("/layout/blank.jsp");
-	    vaadinLayoutContext.setFooter("/layout/blank.jsp");
-	}
-	setContext(request, vaadinLayoutContext);
+	final Context context = useBennuLayout ? new VaadinLayoutContext(getContext(request).getPath()) : new FullVaadinLayoutContext();
+	setContext(request, context);
 	return forward(request, "/embedded/vaadin-embedded.jsp");
     }
 }
