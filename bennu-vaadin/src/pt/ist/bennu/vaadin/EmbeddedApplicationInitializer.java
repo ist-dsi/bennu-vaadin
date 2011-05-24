9a55a37d17040a578fb8ef414c0f8626099016a4
Luis Cruz <luis.cruz@ist.utl.pt>
2011-05-24 19:06:03 +0000

    Refactor: Added custom error handleing capability to bennu-vaadin based apps.

--- bennu-vaadin/src/pt/ist/bennu/vaadin/EmbeddedApplicationInitializer.java
+++ bennu-vaadin/src/pt/ist/bennu/vaadin/EmbeddedApplicationInitializer.java
@@ -11,6 +11,9 @@ import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
+import myorg._development.PropertiesManager;
+import pt.ist.bennu.vaadin.errorHandleing.VirtualHostAwareErrorHandler;
+import pt.ist.vaadinframework.ApplicationErrorListener;
 import pt.ist.vaadinframework.EmbeddedApplication;
 import pt.ist.vaadinframework.ui.EmbeddedComponentContainer;
 import pt.utl.ist.fenix.tools.util.FileUtils;
@@ -41,9 +44,9 @@ public class EmbeddedApplicationInitializer extends HttpServlet {
 		    throw new Error("Error interpreting pattern: " + path, e);
 		}
 	    }
-
 	}
 
+	initErrorListener();
     }
 
     private void loadEmbeddedComponentsFromFile(final Set<Class<? extends EmbeddedComponentContainer>> embeddedComponentClasses) {
@@ -67,6 +70,27 @@ public class EmbeddedApplicationInitializer extends HttpServlet {
 	} else {
 	    throw new Error("Error opening file: " + EmbeddedAnnotationProcessor.LOG_FILENAME);
 	}
+    }
+
+    private void initErrorListener() {
+	final String errorListenerClassName = PropertiesManager.getProperty("error.listener.vaadin.class.name");
+	final ApplicationErrorListener errorListener;
+	if (errorListenerClassName == null || errorListenerClassName.isEmpty()) {
+	    System.out.println("Init virtual host aware...");
+	    errorListener = new VirtualHostAwareErrorHandler();
+	} else {
+	    try {
+		final Class errorListenerClass = Class.forName(errorListenerClassName);
+		errorListener = (ApplicationErrorListener) errorListenerClass.newInstance();
+	    } catch (final ClassNotFoundException e) {
+		throw new Error(e);
+	    } catch (final InstantiationException e) {
+		throw new Error(e);
+	    } catch (final IllegalAccessException e) {
+		throw new Error(e);
+	    }
+	}
+	EmbeddedApplication.registerErrorListener(errorListener);
     }
 
 }
---
+++ bennu-vaadin/src/pt/ist/bennu/vaadin/errorHandleing/VirtualHostAwareErrorHandler.java
@@ -0,0 +1,48 @@
+package pt.ist.bennu.vaadin.errorHandleing;
+
+import java.net.SocketException;
+
+import org.apache.log4j.Logger;
+
+import pt.ist.vaadinframework.ApplicationErrorListener;
+import pt.ist.vaadinframework.UserErrorException;
+import pt.ist.vaadinframework.VaadinFrameworkLogger;
+
+import com.vaadin.Application;
+import com.vaadin.data.Validator.InvalidValueException;
+import com.vaadin.terminal.Terminal.ErrorEvent;
+import com.vaadin.ui.Window.Notification;
+
+public class VirtualHostAwareErrorHandler implements ApplicationErrorListener {
+
+    @Override
+    public void terminalError(final ErrorEvent event, final Application application) {
+	System.out.println("handleing....");
+	final Logger logger = VaadinFrameworkLogger.getLogger();
+
+	final Throwable throwable = event.getThrowable();
+	if (throwable instanceof SocketException) {
+	    // Most likely client browser closed socket
+	    logger.info("SocketException in CommunicationManager. Most likely client (browser) closed socket.");
+	    return;
+	}
+
+	if (throwable instanceof InvalidValueException
+		|| (throwable.getCause() != null && throwable.getCause() instanceof InvalidValueException)) {
+	    // ignore, validation errors are handled by the fields
+	} else if (throwable instanceof UserErrorException) {
+	    application.getMainWindow().showNotification(throwable.getLocalizedMessage(), Notification.TYPE_ERROR_MESSAGE);
+	} else if (throwable.getCause() != null && throwable.getCause() instanceof UserErrorException) {
+	    application.getMainWindow().showNotification(throwable.getCause().getLocalizedMessage(), Notification.TYPE_ERROR_MESSAGE);
+	} else {
+	    final TerminalErrorWindow terminalErrorWindow = createTerminalErrorWindow(event);
+	    application.getMainWindow().addWindow(terminalErrorWindow);
+	    logger.error("Terminal error:", throwable);
+	}
+    }
+
+    protected TerminalErrorWindow createTerminalErrorWindow(final ErrorEvent event) {
+	return new TerminalErrorWindow(event);
+    }
+
+}
