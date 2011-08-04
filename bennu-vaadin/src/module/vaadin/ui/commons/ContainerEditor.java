5254434b9f9a72103d9ce25605df32ed8b30d71a
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2011-08-04 09:18:04 +0000

    New Feature: order management moved to ContainerEditor

--- bennu-vaadin/src/module/vaadin/ui/commons/ContainerEditor.java
+++ bennu-vaadin/src/module/vaadin/ui/commons/ContainerEditor.java
@@ -1,5 +1,6 @@
 package module.vaadin.ui.commons;
 
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import module.vaadin.data.util.BufferedContainer;
@@ -9,13 +10,17 @@ import module.vaadin.resources.VaadinResources;
 import org.apache.commons.lang.StringUtils;
 
 import com.vaadin.data.Container;
+import com.vaadin.data.Container.Indexed;
 import com.vaadin.data.Container.ItemSetChangeEvent;
 import com.vaadin.data.Property;
+import com.vaadin.terminal.ThemeResource;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
+import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.FieldWrapper;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Table;
 import com.vaadin.ui.Table.ColumnGenerator;
 import com.vaadin.ui.TableFieldFactory;
@@ -23,6 +28,46 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.themes.BaseTheme;
 
 public class ContainerEditor<PC> extends FieldWrapper<PC> {
+    public static class OrderChanger extends CustomComponent {
+
+	private final Button up = new Button();
+
+	private final Button down = new Button();
+
+	public OrderChanger(final Indexed container, final Object itemId) {
+	    HorizontalLayout layout = new HorizontalLayout();
+	    layout.addComponent(up);
+	    up.setIcon(new ThemeResource("../runo/icons/32/arrow-up.png"));
+	    up.addStyleName(BaseTheme.BUTTON_LINK);
+	    up.addListener(new ClickListener() {
+		@Override
+		public void buttonClick(ClickEvent event) {
+		    int newIndex = container.indexOfId(itemId) - 1;
+		    container.removeItem(itemId);
+		    container.addItemAt(newIndex, itemId);
+		    up.setEnabled(newIndex > 0);
+		}
+	    });
+	    up.setEnabled(container.indexOfId(itemId) > 0);
+
+	    layout.addComponent(down);
+	    down.setIcon(new ThemeResource("../runo/icons/32/arrow-down.png"));
+	    down.addStyleName(BaseTheme.BUTTON_LINK);
+	    down.addListener(new ClickListener() {
+		@Override
+		public void buttonClick(ClickEvent event) {
+		    int newIndex = container.indexOfId(itemId) + 1;
+		    container.removeItem(itemId);
+		    container.addItemAt(newIndex, itemId);
+		    down.setEnabled(newIndex < container.size() - 1);
+		}
+	    });
+	    down.setEnabled(container.indexOfId(itemId) < container.size() - 1);
+
+	    setCompositionRoot(layout);
+	}
+    }
+
     public static class ContainerEditorTable extends TransactionalTable {
 	public ContainerEditorTable(TableFieldFactory factory, ResourceBundle bundle) {
 	    super(bundle);
@@ -30,6 +75,12 @@ public class ContainerEditor<PC> extends FieldWrapper<PC> {
 	    setPageLength(0);
 	    setTableFieldFactory(factory);
 	    setEditable(true);
+	    addGeneratedColumn("index", new ColumnGenerator() {
+		@Override
+		public Component generateCell(Table source, Object itemId, Object columnId) {
+		    return new OrderChanger((Indexed) getContainerDataSource(), itemId);
+		}
+	    });
 	    addGeneratedColumn(StringUtils.EMPTY, new ColumnGenerator() {
 		@Override
 		public Component generateCell(final Table source, final Object itemId, Object columnId) {
@@ -62,6 +113,14 @@ public class ContainerEditor<PC> extends FieldWrapper<PC> {
 	public void setPropertyDataSource(Property newDataSource) {
 	    if (newDataSource instanceof Container) {
 		super.setContainerDataSource((Container) newDataSource);
+		ArrayList<Object> columns = new ArrayList<Object>();
+		if (newDataSource instanceof Indexed) {
+		    columns.add("index");
+		    setColumnWidth("index", 77);
+		}
+		columns.addAll(((Container) newDataSource).getContainerPropertyIds());
+		columns.add(StringUtils.EMPTY);
+		setVisibleColumns(columns.toArray(new Object[0]));
 	    }
 	}
     }
