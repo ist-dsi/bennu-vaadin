d210f177b352b41b4c10afec2ad4abe3c94d352c
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2011-10-05 17:59:00 +0000

    Refactor: Participation is now a value type to ease edition. Container editor for field sets

---
+++ bennu-vaadin/src/module/vaadin/ui/BennuTheme.java
@@ -0,0 +1,9 @@
+package module.vaadin.ui;
+
+import pt.ist.bennu.ui.BennuConstants;
+
+public class BennuTheme extends BennuConstants {
+    public static final String WIDE_FORM = "wide";
+
+    public static final String TABLE_CLEAR = TABLE_BORDERLESS + " clear";
+}
