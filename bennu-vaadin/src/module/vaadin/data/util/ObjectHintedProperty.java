8cb55b56d245b163e9fc41481b129978c9daf5d1
Sérgio Silva <sergio.silva@ist.utl.pt>
2011-10-05 18:04:59 +0000

    Minor Change: compliant with new HintedProperty interface

--- bennu-vaadin/src/module/vaadin/data/util/ObjectHintedProperty.java
+++ bennu-vaadin/src/module/vaadin/data/util/ObjectHintedProperty.java
@@ -7,24 +7,28 @@ import pt.ist.vaadinframework.data.HintedProperty;
 
 import com.vaadin.data.util.ObjectProperty;
 
-
 public class ObjectHintedProperty<T> extends ObjectProperty<T> implements HintedProperty {
 
     private final Collection<Hint> hints;
-    
+
     public ObjectHintedProperty(Object value, Class<T> type, Collection<Hint> hints) {
 	super(value, type);
 	this.hints = hints;
     }
-    
+
     public ObjectHintedProperty(Object value, Class<T> type, Hint... hints) {
 	super(value, type);
 	this.hints = Arrays.asList(hints);
     }
-    
+
     @Override
     public Collection<Hint> getHints() {
 	return hints;
+    }
+
+    @Override
+    public void addHint(Hint arg0) {
+	hints.add(arg0);
     }
 
 }
