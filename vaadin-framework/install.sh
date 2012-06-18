ad8e08c4b8b6e53d5bf9e9d4e17b57a16d5fe75f
Sérgio Silva <sergio.silva@ist.utl.pt>
2012-06-18 16:09:06 +0000

    Minor Change: Update easyuploads to latest version

--- vaadin-framework/install.sh
+++ vaadin-framework/install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-BENNU_DIR=~/workspace/bennu-core
+BENNU_DIR=~/workspace/git/bennu-core
+mvn clean install
 cp target/vaadin-framework-0.1.jar $BENNU_DIR/modules/bennu-vaadin/web/WEB-INF/lib/
