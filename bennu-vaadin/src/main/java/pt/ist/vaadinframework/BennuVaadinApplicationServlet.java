3b6db979b55a18f36f5e81e6e2289b22447390ff
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2012-09-10 13:56:26 +0000

    New Feature: added use of google fonts

---
+++ bennu-vaadin/src/main/java/pt/ist/vaadinframework/BennuVaadinApplicationServlet.java
@@ -0,0 +1,17 @@
+package pt.ist.vaadinframework;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.vaadin.terminal.gwt.server.ApplicationServlet;
+
+public class BennuVaadinApplicationServlet extends ApplicationServlet {
+    @Override
+    protected void writeAjaxPageHtmlHeader(BufferedWriter page, String title, String themeUri, HttpServletRequest request)
+	    throws IOException {
+	page.write("<link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>");
+	super.writeAjaxPageHtmlHeader(page, title, themeUri, request);
+    }
+}
