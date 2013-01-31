55cea39c5b866fe8108c655a362b0ce6e4f3b2ab
Pedro Santos <pedro.miguel.santos@ist.utl.pt>
2013-01-31 20:35:53 +0000

    Reformatted and enforced code conventions

--- vaadin-embedded/src/main/java/pt/ist/vaadinframework/annotation/EmbeddedAnnotationProcessor.java
+++ vaadin-embedded/src/main/java/pt/ist/vaadinframework/annotation/EmbeddedAnnotationProcessor.java
@@ -41,57 +41,57 @@ import org.apache.commons.io.FileUtils;
 @SupportedAnnotationTypes({ "pt.ist.vaadinframework.annotation.EmbeddedComponent" })
 public class EmbeddedAnnotationProcessor extends AbstractProcessor {
 
-    public static final String LOG_FILENAME = ".embeddedAnnotationLog";
-    public static final String ENTRY_SEPERATOR = "\n";
+	public static final String LOG_FILENAME = ".embeddedAnnotationLog";
+	public static final String ENTRY_SEPERATOR = "\n";
 
-    @Override
-    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+	@Override
+	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
 
-	final Set<String> actions = new HashSet<String>();
+		final Set<String> actions = new HashSet<String>();
 
-	final File file = new File(LOG_FILENAME);
-	if (file.exists()) {
-	    try {
-		final String contents = FileUtils.readFileToString(file);
-		for (final String line : contents.split(ENTRY_SEPERATOR)) {
-		    actions.add(line);
+		final File file = new File(LOG_FILENAME);
+		if (file.exists()) {
+			try {
+				final String contents = FileUtils.readFileToString(file);
+				for (final String line : contents.split(ENTRY_SEPERATOR)) {
+					actions.add(line);
+				}
+			} catch (final IOException e) {
+				e.printStackTrace();
+			}
 		}
-	    } catch (final IOException e) {
-		e.printStackTrace();
-	    }
-	}
 
-	final Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(EmbeddedComponent.class);
-	for (final Element element : elements) {
-	    if (element instanceof TypeElement) {
-		final TypeElement typeElement = (TypeElement) element;
-		actions.add(typeElement.getQualifiedName().toString());
-	    } else {
-		System.out.println("Skipping processing of element: " + element.getClass().getName()
-			+ ", this type was not expected!");
-	    }
-	}
+		final Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(EmbeddedComponent.class);
+		for (final Element element : elements) {
+			if (element instanceof TypeElement) {
+				final TypeElement typeElement = (TypeElement) element;
+				actions.add(typeElement.getQualifiedName().toString());
+			} else {
+				System.out.println("Skipping processing of element: " + element.getClass().getName()
+						+ ", this type was not expected!");
+			}
+		}
 
-	FileWriter fileWriter = null;
-	try {
-	    fileWriter = new FileWriter(LOG_FILENAME, false);
-	    for (final String action : actions) {
-		fileWriter.append(action);
-		fileWriter.write(ENTRY_SEPERATOR);
-	    }
-	} catch (final IOException e) {
-	    e.printStackTrace();
-	} finally {
-	    if (fileWriter != null) {
+		FileWriter fileWriter = null;
 		try {
-		    fileWriter.close();
+			fileWriter = new FileWriter(LOG_FILENAME, false);
+			for (final String action : actions) {
+				fileWriter.append(action);
+				fileWriter.write(ENTRY_SEPERATOR);
+			}
 		} catch (final IOException e) {
-		    e.printStackTrace();
+			e.printStackTrace();
+		} finally {
+			if (fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-	    }
-	}
 
-	return true;
-    }
+		return true;
+	}
 
 }
